5473c5cf9774a3611216d349a0dd19bec268cafc
abr21 <abr211096@gmail.com>
2022-08-15 12:23:21 +0200

    task

--- app/src/main/java/com/example/retrofitv1/pojo/Entity.kt
+++ app/src/main/java/com/example/retrofitv1/pojo/Entity.kt
@@ -9,5 +9,5 @@ data class Entity (
     @SerializedName("status") val status:String,
     @SerializedName("totalResults") val totalResults:Int,
     @SerializedName("articles") var articles:ArrayList<Article>
-
+//smth
         )
